21b8b6dddbe77cfe1fde6c5766316f82a88362c0
GoldenRodger5 <IsaacMineo@gmail.com>
2025-06-28 14:38:26 -0400

    completed code

--- app.py
+++ app.py
@@ -244,6 +244,7 @@ async def get_hint(request: HintRequest):
     
     return {'hint': hint}
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
